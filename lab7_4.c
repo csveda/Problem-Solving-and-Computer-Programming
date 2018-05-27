e601c4b36fd9b5b2b7bb81c78f874c18d97904a0
Ramesh Ragala <rameshragala@gmail.com>
2018-05-27 15:23:41 +0530

    Add files via upload

---
+++ lab7_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+   int i, j, n;
+   n = 4;
+   j = 1;
+   for(i = n; i <= (n*10); i+=n) 
+	{
+	      printf("%3d  x %2d  =  %3d\n", n, j, i);
+	      j++;
+   	}
+
+   return 0;
+}
